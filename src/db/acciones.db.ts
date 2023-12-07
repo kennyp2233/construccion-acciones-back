92cda53c8c0c5811614943960f5d420eb9791f11
kennyp2233 <kennyp41234@gmail.com>
2023-12-06 22:05:21 -0500

    configuracion extra bd

--- src/db/acciones.db.ts
+++ src/db/acciones.db.ts
@@ -9,6 +9,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: "mysql",
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
     logging: console.log,
 });
 
